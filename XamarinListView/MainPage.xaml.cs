56d66e8a8885532c392567ae8bf8aebed706e046
Markos Ayala <elmarkos23@gmail.com>
2019-02-08 11:17:42 -0500

    .

--- XamarinListView/MainPage.xaml.cs
+++ XamarinListView/MainPage.xaml.cs
@@ -12,11 +12,24 @@ namespace XamarinListView
         public MainPage()
         {
             InitializeComponent();
+
+            List<Persona> lst = new List<Persona>();
+            lst.Add(new Persona { nombre = "Marco" });
+            lst.Add(new Persona { nombre = "Miguel" });
+            lst.Add(new Persona { nombre = "Antonio" });
+            lst.Add(new Persona { nombre = "Belen" });
+            lvDatos.ItemsSource = lst;
         }
 
         private async void Opcion1_Clicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Mensaje","Hola","OK");
+            await DisplayAlert("Mensaje","Hola 1","OK");
+            lvDatos.SelectionMode = ListViewSelectionMode.None;
+        }
+
+        async void Opcion2_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje", "Hola 2", "OK");
         }
     }
 }
---
+++ XamarinListView/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinListView
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+    }
+}
