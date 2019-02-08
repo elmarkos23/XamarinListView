99cb8b262262ecd0f190b96a4257615cac60f097
Markos Ayala <elmarkos23@gmail.com>
2019-02-08 10:55:25 -0500

    Initial commit

---
+++ XamarinListView/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Opcion1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje","Hola","OK");
+        }
+    }
+}
